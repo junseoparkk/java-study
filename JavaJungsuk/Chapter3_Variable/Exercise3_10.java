487468249654ef9bfaacee7512c565129761e517
Junseo Park <wnstj444@naver.com>
2023-02-17 19:03:13 +0900

    first commit

---
+++ JavaJungsuk/Chapter3_Variable/Exercise3_10.java
@@ -0,0 +1,12 @@
+public class Exercise3_10 {
+
+	public static void main(String[] args) {
+		char ch = 'A';
+		
+		char lowerCase = (char) (('A'<=ch && ch<='Z')?(ch+32):ch);
+		
+		System.out.println("ch: "+ch);
+		System.out.println("ch to lowerCase: "+lowerCase);
+	}
+
+}
